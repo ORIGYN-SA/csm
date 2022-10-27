bb9e04cf3eb6fcfc8f77d09bf8f819af056a70fc
Motokoder <motokoder@tutanota.com>
2022-10-27 16:11:06 -0600

    SI-122 remove Secp256k1 pk code

--- src/methods/identity.ts
+++ src/methods/identity.ts
@@ -10,24 +10,20 @@ import fs from 'fs';
 import path from 'path';
 import { Identity } from '@dfinity/agent';
 import hdkey from 'hdkey';
-import sha256 from 'sha256';
 import pemfile from 'pem-file';
 import { mnemonicToSeed } from 'bip39';
 import { Secp256k1KeyIdentity, Ed25519KeyIdentity } from '@dfinity/identity';
 import { log } from './logger';
 
+// Loads an Ed25519 private key or a Secp256k1 seed phrase from a file and creates
+// an Identity that can be used in actor references to call canister functions.
 export async function getIdentity(keyFilePath: string): Promise<Identity> {
   const contents = fs.readFileSync(keyFilePath).toString();
   const fileExt = path.extname(keyFilePath).toLowerCase();
 
   if (fileExt === '.pem') {
-    if (contents.indexOf('BEGIN EC PRIVATE KEY') > -1) {
-      const rawBuffer = Uint8Array.from(contents as any).buffer;
-      const privateKey = Uint8Array.from(sha256(rawBuffer as any, { asBytes: true }));
-      const identity = Secp256k1KeyIdentity.fromSecretKey(privateKey);
-      log(`Loaded Secp256k1 identity ${identity.getPrincipal()} from .pem file ${keyFilePath}.`);
-      return identity;
-    } else if (contents.indexOf('BEGIN PRIVATE KEY')) {
+    if (contents.indexOf('BEGIN PRIVATE KEY')) {
+      // Try to load Ed25519 private key
       var buf = pemfile.decode(contents);
       if (buf.length != 85) {
         throw 'expecting byte length 85 but got ' + buf.length;
@@ -40,7 +36,7 @@ export async function getIdentity(keyFilePath: string): Promise<Identity> {
       throw 'Could not recognize the private key type.';
     }
   } else if ((contents || '').split(' ').length === 12) {
-    // seed file
+    // Try to load Secp256k1 seed file
     let seed: Buffer = await mnemonicToSeed(contents);
     const root = hdkey.fromMasterSeed(seed);
     const addrnode = root.derive("m/44'/223'/0'/0/0");
@@ -48,7 +44,7 @@ export async function getIdentity(keyFilePath: string): Promise<Identity> {
     log(`Loaded identity ${identity.getPrincipal()} from seed phrase file ${keyFilePath}.`);
     return identity;
   } else {
-    log(`Invalid seed phrase file ${keyFilePath}`);
-    throw 'Invalid seed phrase';
+    const err = `Could not load identity from ${keyFilePath}`;
+    throw err;
   }
 }
