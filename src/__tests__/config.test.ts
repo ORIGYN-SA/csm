9a6b4ce3cc054ed20e1a1bf4aed4b2e0f9516e02
Motokoder <motokoder@tutanota.com>
2022-10-31 07:47:34 -0600

    Revert "SI-163 added royalties to unit tests"
    This reverts commit 4ddf539d44881d067c5ac5af0b5ff4bfd3cb4639.

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -4,11 +4,11 @@ import { ConfigArgs } from '../types/config';
 
 const BUILD_FOLDER = '../../lib';
 const inputArgvShortNames =
-  '["node","projects/csm.js","config","-c","bm","-d","Brain Matters","-t","bm-","-n","com.brainmatters","-i","aaaaa-bbbbb-ccccc-ddddd-cai","-p","11111-22222-33333-44444-55555-66666-77777-88888-99999-11111-222","-s","true","-f","./projects/bm/__temp","-m","primary:nft*.png, preview:index.html, hidden:mystery.gif","-o","aaaaa-bbbbb-ccccc-ddddd-eeeee-fffff-ggggg-iiiii-jjjjj-kkkkk-lll","-s","true","--brokerRoyalty","0.05","--customRoyalty","0.02","--origynatorRoyalty","0.03","-q","5,5,5,5,1,1,1"]';
+  '["node","projects/csm.js","config","-c","bm","-d","Brain Matters","-t","bm-","-n","com.brainmatters","-i","aaaaa-bbbbb-ccccc-ddddd-cai","-p","11111-22222-33333-44444-55555-66666-77777-88888-99999-11111-222","-s","true","-f","./projects/bm/__temp","-m","primary:nft*.png, preview:index.html, hidden:mystery.gif","-o","aaaaa-bbbbb-ccccc-ddddd-eeeee-fffff-ggggg-iiiii-jjjjj-kkkkk-lll","-s","true","-q","5,5,5,5,1,1,1"]';
 const inputArgvShortNamesOmitOptional =
   '["node","projects/csm.js","config","-c","bm","-d","Brain Matters","-t","bm-","-n","com.brainmatters","-i","rrkah-fqaaa-aaaaa-aaaaq-cai","-p","6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe","-f","./projects/bm/__temp","-m","primary:nft*.png, preview:index.html, hidden:mystery.gif"]';
 const inputArgvLongNames =
-  '["node","projects/csm.js","config","--collectionId","bm","--collectionDisplayName","Brain Matters","--tokenPrefix","bm_","--nftCanisterId","rrkah-fqaaa-aaaaa-aaaaq-cai","--creatorPrincipal","6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe","--namespace","ogy.brainmatters","--folderPath","./projects/simple-video/assets","--assetMappings","primary:mike.png, experience:Origynator_Card_Mike_Schwartz_Small.mp4, preview:index.html","--soulbound","false","--brokerRoyalty","0.05","--customRoyalty","0.02","--origynatorRoyalty","0.03","--nftQuantities","3"]';
+  '["node","projects/csm.js","config","--collectionId","bm","--collectionDisplayName","Brain Matters","--tokenPrefix","bm_","--nftCanisterId","rrkah-fqaaa-aaaaa-aaaaq-cai","--creatorPrincipal","6i6da-t3dfv-vteyg-v5agl-tpgrm-63p4y-t5nmm-gi7nl-o72zu-jd3sc-7qe","--namespace","ogy.brainmatters","--folderPath","./projects/simple-video/assets","--assetMappings","primary:mike.png, experience:Origynator_Card_Mike_Schwartz_Small.mp4, preview:index.html","--soulbound","false","--nftQuantities","3"]';
 
 describe('config module', () => {
   describe('getArgValue', () => {
@@ -46,9 +46,6 @@ describe('config module', () => {
         assetMappings: 'primary:nft*.png, preview:index.html, hidden:mystery.gif',
         nftOwnerId: 'aaaaa-bbbbb-ccccc-ddddd-eeeee-fffff-ggggg-iiiii-jjjjj-kkkkk-lll',
         soulbound: 'true',
-        brokerRoyalty: "0.05",
-        customRoyalty: "0.02",
-        origynatorRoyalty: "0.03",
         nftQuantities: '5,5,5,5,1,1,1',
       };
 
@@ -69,9 +66,7 @@ describe('config module', () => {
         assetMappings: 'primary:nft*.png, preview:index.html, hidden:mystery.gif',
 
         // optional arguments
-        brokerRoyalty: '',
-        customRoyalty: '',
-        origynatorRoyalty: '',
+
         // should default to nftCanisterId
         nftOwnerId: 'rrkah-fqaaa-aaaaa-aaaaq-cai',
         // should default to 'false'
